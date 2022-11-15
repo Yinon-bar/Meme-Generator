e38f0003ef34f1a2723d31710e1cc59f31baf028
Yinon bar <yinonbar1988@gmail.com>
2022-11-15 15:41:18 +0200

    clean slate

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -4,8 +4,10 @@ import trollImg from "../../src/Assets/Troll Face.png";
 function Header() {
   return (
     <header className="Header">
-      <img className="img" src={trollImg} alt="" />
-      <h2 className="headerTitle">Header</h2>
+      <div className="logo">
+        <img className="img" src={trollImg} alt="" />
+        <h2 className="headerTitle">Meme Generator 2022</h2>
+      </div>
       <h4 className="headerPtoject">React Course - Project 3</h4>
     </header>
   );
