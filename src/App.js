f6f6c0399e7a2014894053d73576d2286902aa64
Yinon bar <yinonbar1988@gmail.com>
2022-11-15 15:08:08 +0200

    clean slate

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import "./App.css";
 import Header from "./Components/Header";
 
 function App() {
-  let a = 5;
   return (
     <div className="App">
       <Header />
--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,5 +1,14 @@
+import "./Header.css";
+import trollImg from "../../src/Assets/Troll Face.png";
+
 function Header() {
-  return <div className="Header">Header</div>;
+  return (
+    <header className="Header">
+      <img src={trollImg} width="60px" alt="" />
+      <h2>Header</h2>
+      <h4>React Course - Project 3</h4>
+    </header>
+  );
 }
 
 export default Header;
