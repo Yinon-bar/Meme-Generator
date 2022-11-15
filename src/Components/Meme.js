030efbf2bf3e1cd97ee90654afffb0361adc2b8f
Yinon Bar <yinonbar1988@gmail.com>
2022-11-15 23:18:21 +0200

    css only

--- src/Components/Meme.js
+++ src/Components/Meme.js
@@ -1,12 +1,19 @@
+import { useEffect } from "react";
 import "./Meme.css";
 
 function Meme() {
+  useEffect(() => {
+    fetch("http://api.imgflip.com/get_memes")
+      .then((resp) => resp.json())
+      .then((data) => console.log(data.data.memes));
+  }, []);
+
   return (
     <main>
       <form className="form">
         <input type="text" defaultValue="Input text" />
         <input type="text" defaultValue="Input text" />
-        <button>Get a new meme image 😂</button>
+        <button onClick={console.log("inon")}>Get a new meme image 😂</button>
       </form>
     </main>
   );
